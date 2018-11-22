46dbe766e91bf4a9e6bbde5ed2a6880f3730ddc2
mk-hill <40850298+mk-hill@users.noreply.github.com>
2018-11-22 15:03:49 -0600

    Add battle forms

--- app/components/Battle.jsx
+++ app/components/Battle.jsx
@@ -1,11 +1,103 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class Battle extends Component {
+class PlayerInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+    };
+  }
+
+  handleChange = event => {
+    const value = event.target.value;
+
+    this.setState({ username: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.props.id, this.state.username);
+  };
+
   render() {
     return (
-      <div>
-        <p>BATTLE!</p>
-      </div>
+      <form className="column" onSubmit={this.handleSubmit}>
+        <label htmlFor="username" className="header">
+          {this.props.label}
+        </label>
+        <input
+          type="text"
+          id="username"
+          placeholder="Github username"
+          autoComplete="off"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <button
+          type="submit"
+          className="button"
+          disabled={!this.state.username}
+        >
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+class Battle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null,
+    };
+
+    // this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit = (id, username) => {
+    this.setState(() => {
+      const newState = {};
+      newState[`${id}Name`] = username;
+      newState[`${id}Image`] = `https://github.com/${username}.png?size=200`;
+      return newState;
+    });
+  };
+
+  render() {
+    const { playerOneName, playerTwoName } = this.state;
+    return (
+      <>
+        <div className="row">
+          {!playerOneName ? (
+            <PlayerInput
+              id="playerOne"
+              label="Player One"
+              onSubmit={this.handleSubmit}
+            />
+          ) : null}
+          {!playerTwoName ? (
+            <PlayerInput
+              id="playerTwo"
+              label="Player Two"
+              onSubmit={this.handleSubmit}
+            />
+          ) : null}
+        </div>
+      </>
     );
   }
 }
